e0c41151cd6916320e850db483655ef13dcf297d
Lonny Snyder <lonnysnyder81@gmail.com>
2017-07-25 17:57:00 -0500

    debugging jsoup practice exercise

--- app/src/main/java/com/example/lonny/myfreehsapp/MainActivity.java
+++ app/src/main/java/com/example/lonny/myfreehsapp/MainActivity.java
@@ -3,11 +3,38 @@ package com.example.lonny.myfreehsapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Document doc;
+        try {
+
+            // need http protocol
+            doc = Jsoup.connect("http://google.com").get();
+
+            // get page title
+            String title = doc.title();
+
+
+            // get all links
+            Elements links = doc.select("a[href]");
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
